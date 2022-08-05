d7bf39193f06e8491e5f8931d56194cc782fadc0
kwanso-naseer <naseer.ahmed@kwanso.com>
2022-08-05 10:25:35 +0500

    [login singup ]

--- models/Todo.js
+++ models/Todo.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const User = require("./User");
 
 const TodoSchema = new mongoose.Schema({
   title: String,
-  description: String
+  description: String,
+  userId: { type: mongoose.Types.ObjectId, ref: User },
 });
 
-module.exports = mongoose.model('Todo', TodoSchema); 
+module.exports = mongoose.model('Todo', TodoSchema);
--- src/app.js
+++ src/app.js
@@ -1,46 +1,43 @@
 const express = require('express')
+const bcrypt = require("bcrypt");
 const app = express()
 const port = 3000
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const Todo = require('../models/Todo')
+const User = require("../models/User");
+
+
+const SECRET_JWT_CODE = 'psmR3Hu0ihHkqZymo1m'
 
 app.use(express.json())
 
 mongoose.connect('mongodb+srv://kwnaso-naseer:Cinnova123@cluster0.kfywi.mongodb.net/mern-todo?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false
 })
   .then(() => console.log('connected db'))
   .catch((err) => console.log(err));
 
-// const todoList = [
-//   {
-//     id: 1,
-//     title: 'Learn Node.js',
-//     description: 'Learn Node.js by building a todo list app',
-//   },
-//   {
-//     id: 2,
-//     title: 'Learn React',
-//     description: 'Learn React by building a todo list app',
-//   },
-//   {
-//     id: 3,
-//     title: 'Learn React Native',
-//     description: 'Learn React Native by building a todo list app',
-//   },
-//   {
-//     id: 4,
-//     title: 'Learn GraphQL',
-//     description: 'Learn GraphQL by building a todo list app',
-//   }
-// ]
+app.get("/user-todo/:id", async (req, res) => {
+  const userId = req.params.id;
+  res.send(await Todo.find({
+    userId
+  }).populate('userId'))
+
+})
 
 app.get('/', async (req, res) => {
   try {
-    res.send(await Todo.find())
+    res.send(await Todo.find().populate("userId"))
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/registered-users', async (req, res) => {
+  try {
+    res.send(await User.find())
   } catch (error) {
     console.log(error)
   }
@@ -55,17 +52,18 @@ app.get('/:id', async (req, res) => {
   }
 })
 
-app.delete('/:id', (req, res) => {
+app.delete('/:id', async (req, res) => {
+  const id = req.params.id;
   try {
-    const removeIndex = Todo.findIndex(item => item.id === parseInt(req.params.id));
-    if (removeIndex === -1) {
-      return res.status(404).send({ message: 'No todo found with this id' })
-    }
-    Todo.splice(removeIndex, 1);
-    res.send(Todo)
+    Todo.findByIdAndRemove(id, function (err) {
+      if (!err) {
+        res.send("Successfully deleted")
+      } else
+        res.send('no id exist')
+    })
   }
   catch (error) {
-    res.status(500).send({ message: error })
+    res.status(500).send({ message: "Server Error" })
   }
 })
 
@@ -100,6 +98,49 @@ app.patch('/:id', async (req, res) => {
     res.status(500).send({ message: 'Server error' })
   }
 
+})
+
+app.post("/signup", async (req, res) => {
+  const body = req.body;
+  console.log(body.email, body.password)
+  const alreadyExist = await User.findOne({ email: body.email })
+  if (alreadyExist) {
+    return res.send("User already exist")
+  }
+
+  const newUser = new User(body)
+  const salt = await bcrypt.genSalt(10)
+
+  newUser.password = await bcrypt.hash(newUser.password, salt)
+  newUser.save().then((newUser) => res.status(201).send(newUser))
+})
+
+app.post("/login", async (req, res) => {
+  const body = req.body;
+  const user = await User.findOne({ email: body.email })
+
+  if (user) {
+    const validatePassword = await bcrypt.compare(body.password, user.password)
+
+    if (validatePassword) {
+      const token = jwt.sign({ id: user._id, email: user.email }, SECRET_JWT_CODE)
+      res.status(200).send(token)
+    } else {
+      res.status(200).send({ message: "Invalid password" });
+    }
+  }
+})
+
+app.post('/add-new-todo', async (req, res) => {
+  const body = req.body;
+  console.log(body.id, body.title, body.description)
+
+  const todo = await Todo.create({
+    userId: body.id,
+    title: body.title,
+    description: body.description
+  })
+  res.send(todo)
 })
 
 app.listen(port, () => {
